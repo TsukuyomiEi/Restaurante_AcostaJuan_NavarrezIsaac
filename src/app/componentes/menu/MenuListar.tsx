dfb8c2ecb58fc94771e2647ca2684c295e9fb202
lgutierr62 <lgutierr62@cuc.edu.co>
2024-11-14 12:08:31 -0500

    CRUD - Listar Platos

--- src/app/componentes/menu/MenuListar.tsx
+++ src/app/componentes/menu/MenuListar.tsx
@@ -1,4 +1,19 @@
+import { useState } from "react";
+import { Plato } from "../../modelos/Plato";
+import { ARREGLO_PLATOS } from "../../mocks/Plato-mocks";
+import { ARREGLO_PLATOS_REGION } from "../../utilidades/dominios/DomRegion";
+
 export const MenuListar = () => {
+  const [arrPlatos] = useState<Plato[]>(ARREGLO_PLATOS);
+
+  const obtenerNombre = (valor: string)=>{
+    for(const objRegion of ARREGLO_PLATOS_REGION){
+      if (objRegion.codRegion==valor) {
+        return objRegion.nombreRegion;
+      }
+    }
+  }
+
   return (
     <>
       <div className="pt-5 d-flex justify-content-center">
@@ -6,35 +21,29 @@ export const MenuListar = () => {
           <table className="table table-striped">
             <thead>
               <tr>
-                <th style={{width: "5%"}}>No.</th>
+                <th style={{width: "5%"}}>Código</th>
                 <th style={{width: "30%"}}>Nombre plato</th>
-                <th style={{width: "25%"}}>Acompañamiento</th>
-                <th style={{width: "20%"}} className="text-center">Precio</th>
-                <th style={{width: "20%"}}>Región</th>
+                <th style={{width: "20%"}}>Precio</th>
+                <th style={{width: "25%"}}>Región</th>
+                <th style={{width: "20%"}}>Imagen</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>1</td>
-                <td>Bandeja paisa</td>
-                <td>Con aguacate</td>
-                <td className="text-center">35000</td>
-                <td>Paisa</td>
+
+            {arrPlatos.map((miPla: Plato)=>(
+              <tr key={miPla.codPlato} className="align-middle">
+                <td>{miPla.codPlato}</td>
+                <td>{miPla.nombrePlato}</td>
+                <td>{miPla.precioPlato}</td>
+                <td>{obtenerNombre(miPla.regionPlato)}</td>
+                <td>
+                  <img src={miPla.imagenPlatoBase64} alt={miPla.nombrePlato} className="imagenListado"/>
+                  <br />
+                  {miPla.imagenPlato}
+                </td>
               </tr>
-              <tr>
-                <td>2</td>
-                <td>Ajiaco</td>
-                <td>Con Arroz</td>
-                <td className="text-center">28000</td>
-                <td>Andina</td>
-              </tr>
-              <tr>
-                <td>3</td>
-                <td>Mute Santandereano</td>
-                <td>Con Arroz</td>
-                <td className="text-center">25000</td>
-                <td>Santander</td>
-              </tr>
+            ))}
+   
             </tbody>
           </table>
         </div>
---
+++ src/app/modelos/Plato.tsx
@@ -0,0 +1,17 @@
+export class Plato {
+  public codPlato: number;
+  public nombrePlato: string;
+  public precioPlato: string;
+  public regionPlato: string;
+  public imagenPlato: string;
+  public imagenPlatoBase64: string;
+
+  constructor(codp: number, nomb: string, prec: string, regi: string, imag: string, base: string) {
+    this.codPlato = codp;
+    this.nombrePlato = nomb;
+    this.precioPlato = prec;
+    this.regionPlato = regi;
+    this.imagenPlato = imag;
+    this.imagenPlatoBase64 = base;
+  }
+}
