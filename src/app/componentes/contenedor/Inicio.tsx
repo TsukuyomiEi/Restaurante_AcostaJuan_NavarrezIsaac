a5631b513670b366aac62dc5529aa579ce50d0c8
CompetitiveSplit <narvaez0526@gmail.com>
2024-11-20 20:57:48 -0500

    Agrega div para imagen de fondo

--- src/app/componentes/contenedor/Inicio.tsx
+++ src/app/componentes/contenedor/Inicio.tsx
@@ -1,19 +1,21 @@
 export const Inicio = () => {
-  return (
-    <>
-      <div className="pt-5 d-flex justify-content-center">
-        <div className="col-md-6">
-          <div className="restaurant-card p-5 text-dark rounded-3">
-            <h2>Bienvenidos al Restaurante Fast Food CUC</h2>
-            <p className="description">
-              En nuestro restaurante, podrás disfrutar de los sabores más
-              auténticos de la cocina colombiana. Desde deliciosos platillos tipicos hasta la riquisima banddeja paisa, 
-              cada plato está preparado con ingredientes frescos y tradicionales. ¡Ven y vive la
-              experiencia culinaria Colombiana con nosotros!
-            </p>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="inicio-background">
+                <div className="pt-5 d-flex justify-content-center">
+                    <div className="col-md-6">
+                        <div className="restaurant-card p-5 text-dark rounded-3">
+                            <h2>Bienvenidos al Restaurante Unicosta</h2>
+                            <p className="description">
+                                En nuestro restaurante, podrás disfrutar de los sabores más
+                                auténticos de la cocina colombiana. Desde deliciosos platillos típicos hasta la riquísima bandeja paisa,
+                                cada plato está preparado con ingredientes frescos y tradicionales. ¡Ven y vive la
+                                experiencia culinaria Colombiana con nosotros!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
