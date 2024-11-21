e1a6fc36f9cdf0b6158f480c877459a4644eaacc
CompetitiveSplit <narvaez0526@gmail.com>
2024-11-20 20:58:59 -0500

    Agrega icono "$" y mejoras visuales

--- src/app/componentes/menu/MenuListar.tsx
+++ src/app/componentes/menu/MenuListar.tsx
@@ -14,40 +14,44 @@ export const MenuListar = () => {
     }
   }
 
+
   return (
     <>
       <div className="pt-5 d-flex justify-content-center">
-        <div className="col-md-8">
-          <table className="table table-striped">
+        <div className="col-md-8 shadow-sm p-3 mb-5 bg-white rounded">
+          <table className="table table-striped table-hover table-bordered">
             <thead>
               <tr>
                 <th style={{width: "5%"}}>Código</th>
-                <th style={{width: "30%"}}>Nombre plato</th>
-                <th style={{width: "20%"}}>Precio</th>
-                <th style={{width: "25%"}}>Región</th>
-                <th style={{width: "20%"}}>Imagen</th>
-              </tr>
-            </thead>
-            <tbody>
+    <th style={{width: "30%"}}>Nombre plato</th>
+    <th style={{width: "20%"}}>Precio</th>
+    <th style={{width: "25%"}}>Región</th>
+    <th style={{width: "20%"}}>Imagen</th>
+    </tr>
+    </thead>
+    <tbody>
 
-            {arrPlatos.map((miPla: Plato)=>(
-              <tr key={miPla.codPlato} className="align-middle">
-                <td>{miPla.codPlato}</td>
-                <td>{miPla.nombrePlato}</td>
-                <td>{miPla.precioPlato}</td>
-                <td>{obtenerNombre(miPla.regionPlato)}</td>
-                <td>
-                  <img src={miPla.imagenPlatoBase64} alt={miPla.nombrePlato} className="imagenListado"/>
-                  <br />
-                  {miPla.imagenPlato}
-                </td>
-              </tr>
-            ))}
-   
-            </tbody>
-          </table>
-        </div>
-      </div>
+      {arrPlatos.map((miPla: Plato)=>(
+        <tr key={miPla.codPlato} className="align-middle">
+          <td>{miPla.codPlato}</td>
+          <td>{miPla.nombrePlato}</td>
+          <td> <i className="bi bi-currency-dollar"></i>{miPla.precioPlato}</td>
+          <td>{obtenerNombre(miPla.regionPlato)}</td>
+          <td>
+            <img
+              src={miPla.imagenPlatoBase64}
+              alt={miPla.nombrePlato}
+              className="imagenListado"
+            />
+            <br />
+            {miPla.imagenPlato}
+          </td>
+        </tr>
+      ))}
+    </tbody>
+    </table>
+    </div>
+    </div>
     </>
   );
 };
