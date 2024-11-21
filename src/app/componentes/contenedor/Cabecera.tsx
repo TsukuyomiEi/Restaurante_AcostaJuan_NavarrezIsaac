2a536db34daa1ce61c965964e94acd1d6cc13816
CompetitiveSplit <narvaez0526@gmail.com>
2024-11-20 20:55:17 -0500

    Elimina "search" y agrega clases para estilos

--- src/app/componentes/contenedor/Cabecera.tsx
+++ src/app/componentes/contenedor/Cabecera.tsx
@@ -1,16 +1,15 @@
 import { NavLink } from "react-router-dom";
-import miLogo from "../../../assets/img/logoReact.png";
+import miLogo from "../../../assets/restaurant.png";
 
 export const Cabecera = () => {
   return (
     <>
       <nav
-        className="navbar navbar-expand-lg bg-dark border-bottom border-body"
-        data-bs-theme="dark"
+        className="navbar navbar-expand-lg navbar-custom"
       >
         <div className="container-fluid">
           <NavLink className="navbar-brand" to="/">
-            <img src={miLogo} alt="El Logo" />
+            <img src={miLogo} alt="El Logo" className="navbar-logo" />
           </NavLink>
           <button
             className="navbar-toggler"
@@ -26,7 +25,7 @@ export const Cabecera = () => {
           <div className="collapse navbar-collapse" id="navbarColor01">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <NavLink className="nav-link active" aria-current="page" to="/">
+                <NavLink className="nav-link" aria-current="page" to="/">
                   Inicio
                 </NavLink>
               </li>
@@ -44,17 +43,17 @@ export const Cabecera = () => {
                   data-bs-toggle="dropdown"
                   aria-expanded="false"
                 >
-                  Menus
+                  Menús
                 </a>
-                <ul className="dropdown-menu">
+                <ul className="dropdown-menu animate-dropdown">
                   <li>
                     <NavLink className="dropdown-item" to="/mencre">
-                      Registrar Menus
+                      Registrar Menús
                     </NavLink>
                   </li>
                   <li>
                     <NavLink className="dropdown-item" to="/menlis">
-                      Listar Menus
+                      Listar Menús
                     </NavLink>
                   </li>
                   <li>
@@ -62,61 +61,20 @@ export const Cabecera = () => {
                   </li>
                   <li>
                     <NavLink className="dropdown-item" to="/menadmin">
-                      Administrar Menus
+                      Administrar Menús
                     </NavLink>
                   </li>
                 </ul>
               </li>
-
               <li className="nav-item">
                 <NavLink className="nav-link" to="/menuacer">
                   Acerca de
                 </NavLink>
               </li>
             </ul>
-            <form className="d-flex" role="search">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-light" type="submit">
-                Search
-              </button>
-            </form>
           </div>
         </div>
       </nav>
-
-      {/*<nav className="navbar navbar-expand-lg" style={{backgroundColor: "#e3f2fd"}} data-bs-theme="light">
-    <div className="container-fluid">
-      <a className="navbar-brand" href="#">Navbar</a>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarColor03">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Features</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Pricing</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">About</a>
-          </li>
-        </ul>
-        <form className="d-flex" role="search">
-          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-          <button className="btn btn-outline-primary" type="submit">Search</button>
-        </form>
-      </div>
-    </div>
-  </nav>*/}
     </>
   );
 };
